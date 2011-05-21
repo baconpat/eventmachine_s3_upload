80af0192bff74311ec4d988d8118eb5281bf909f
Patrick Bacon <bacon@atomicobject.com>
2011-05-21 16:39:23 -0400

    Two scripts: one for http (async and fast) and one for https (blocks and slow)

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'http://rubygems.org'
+
+gem 'happening', "0.2.0"
---
+++ lib/https_upload.rb
@@ -0,0 +1,24 @@
+require "rubygems"
+require "bundler/setup"
+require 'happening'
+require 'yaml'
+
+s3_config = YAML.load_file("s3.yml")
+
+EM.run do 
+  EventMachine::add_periodic_timer(1) { print "." }
+  
+  item = Happening::S3::Item.new(s3_config['bucket'],
+                                 "25mb_file", 
+                                 :protocol => "https",
+                                 :aws_access_key_id => s3_config['access_key_id'],
+                                 :aws_secret_access_key => s3_config['secret_access_key'],
+                                 :permissions => 'public-read')
+  
+  file_contents = File.read("data/25mb_file")
+  start_time = Time.now
+  item.put file_contents do |response|
+    puts "Finished in #{Time.now - start_time} seconds"
+    EM.stop
+  end
+end
